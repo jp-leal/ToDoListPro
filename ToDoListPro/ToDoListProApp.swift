2551835aa500bcafe9c654d832d5ac2102bf1ac6
joaobug <joaoenfrance@gmail.com>
2024-04-17 17:02:13 -0300

    Initial Commit

---
+++ ToDoListPro/ToDoListProApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListProApp.swift
+//  ToDoListPro
+//
+//  Created by Joao Leal on 4/17/24.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
