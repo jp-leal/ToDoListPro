cb8766613ad0ca4261912ce60a2ee31012e36634
joaobug <joaoenfrance@gmail.com>
2024-04-18 18:16:08 -0300

    working

--- ToDoListPro/DetailView.swift
+++ ToDoListPro/DetailView.swift
@@ -11,26 +11,44 @@ struct DetailView: View {
     
     var passedValue: String
     @Environment (\.dismiss) private var dismiss
-    
+    @State private var toDo = ""
+    @State private var reminderIsOn = false
+    @State private var dueData = Date.now + (60*60*24)
+    @State private var notes = ""
+    @State private var isCompleted = false
     var body: some View {
-        VStack{
-            Image(systemName: "swift")
-                .resizable()
-                .scaledToFit()
-                .foregroundStyle(.orange)
+        List{
+            TextField("Enter To Do here", text: $toDo)
+                .font(.title)
+                .textFieldStyle(.roundedBorder)
+                .padding(.vertical)
             
-            Text("Legend \(passedValue)")
-                .font(.largeTitle)
-                .multilineTextAlignment(.center)
-Spacer()
+            Toggle("Set Reminder", isOn: $reminderIsOn)
+            DatePicker("Date", selection: $dueData)
+                .disabled(!reminderIsOn)
             
-            Button("Get Back!") {
-                dismiss()
-            }
+            Text("Notes: ")
+                .padding(.top)
+            TextField("Notes", text: $notes, axis: .vertical)
+                .textFieldStyle(.roundedBorder)
+                .listRowSeparator(.hidden)
+            
+            Toggle("Done", isOn: $isCompleted)
+                .padding(.top)
+            
             
         }
-        .padding()
-        .navigationBarBackButtonHidden()
+        .listStyle(.plain)
+        .toolbar {
+            ToolbarItem(placement: .topBarLeading){ Button("Cancel") {
+                dismiss()
+            }
+            }
+            ToolbarItem(placement: .topBarTrailing){ Button("SAVE") {
+                //ADD SAVE
+            }
+            }
+        }
     }
 }
 
